5f3a025e106777935486cbda465e25db54ac9b44
Daniel Flook <daniel@flook.org>
2021-12-11 12:15:29 +0000

    Day 11

---
+++ 2021/11/part2.py
@@ -0,0 +1,88 @@
+from __future__ import annotations
+
+from dataclasses import dataclass, field
+from pprint import pprint
+from typing import List
+
+
+@dataclass
+class Octopus:
+    energy: int
+    flashed: bool = False
+    adjacent: List[Octopus] = field(default_factory=list)
+
+    def __repr__(self):
+        return '!' if self.flashed else str(self.energy)
+
+
+def read_octopi(path):
+    octopi = []
+    with open(path) as f:
+        for line in f.readlines():
+            octopi.append([Octopus(int(energy)) for energy in line.strip()])
+
+    def try_append(octopus: Octopus, octopi: List[List[Octopus]], r: int, c: int):
+        if r < 0 or c < 0:
+            return
+        try:
+            octopus.adjacent.append(octopi[r][c])
+        except IndexError:
+            pass
+
+    for row_num in range(len(octopi)):
+        for col_num in range(len(octopi[row_num])):
+            octopus = octopi[row_num][col_num]
+            try_append(octopus, octopi, row_num - 1, col_num - 1)  # top left
+            try_append(octopus, octopi, row_num - 1, col_num)  # top above
+            try_append(octopus, octopi, row_num - 1, col_num + 1)  # top right
+            try_append(octopus, octopi, row_num, col_num - 1)  # left
+            try_append(octopus, octopi, row_num, col_num + 1)  # right
+            try_append(octopus, octopi, row_num + 1, col_num - 1)  # bottom left
+            try_append(octopus, octopi, row_num + 1, col_num)  # bottom below
+            try_append(octopus, octopi, row_num + 1, col_num + 1)  # bottom right
+
+    return octopi
+
+
+def step(octopi: List[List[Octopus]]) -> int:
+    flash_count = 0
+    flashing = []
+
+    for row in octopi:
+        for octopus in row:
+            octopus.energy += 1
+            if octopus.energy > 9:
+                flashing.append(octopus)
+
+    while flashing:
+        flash_count += 1
+        flasher = flashing.pop()
+        for adjacent_octo in flasher.adjacent:
+            if not adjacent_octo.flashed and adjacent_octo not in flashing:
+                adjacent_octo.energy += 1
+                if adjacent_octo.energy > 9:
+                    flashing.append(adjacent_octo)
+            flasher.flashed = True
+
+    for row in octopi:
+        for octopus in row:
+            if octopus.flashed:
+                octopus.energy = 0
+                octopus.flashed = False
+
+    return flash_count
+
+
+octopi = read_octopi('input.txt')
+print('Before any steps')
+pprint(octopi)
+
+total_flashes = len(octopi) * len(octopi[0])
+step_num = 1
+while step(octopi) != total_flashes:
+    print(f'After step {step_num}')
+    step_num += 1
+    pprint(octopi)
+
+print(f'All flashed in step {step_num}')
+# 237

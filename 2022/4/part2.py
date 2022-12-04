537ff3765d7fda6e77266f04824282d19b84d715
Daniel Flook <daniel@flook.org>
2022-12-04 10:26:28 +0000

    day 4

---
+++ 2022/4/part2.py
@@ -0,0 +1,19 @@
+import string
+from pathlib import Path
+from typing import Set, Tuple, Iterable
+
+def get_assignment(assignment: str) -> set[str]:
+    start, end = assignment.split('-')
+    return set(range(int(start), int(end)+1))
+
+def read(p: str) -> Iterable[Tuple[Set, Set]]:
+    for line in Path(p).read_text().splitlines():
+        left, right = line.split(',')
+        yield get_assignment(left), get_assignment(right)
+
+sum = 0
+for left, right in read('input.txt'):
+    if left.intersection(right):
+        sum += 1
+
+print(sum)

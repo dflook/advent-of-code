ca239281a0d85ad4bfd1b05a2b5b5b3481b305d6
Daniel Flook <daniel@flook.org>
2022-12-05 09:17:29 +0000

    day 5

---
+++ 2022/5/part1.py
@@ -0,0 +1,46 @@
+import re
+import string
+from pathlib import Path
+from typing import Set, Tuple, Iterable
+
+def read(p: str) -> Tuple[list[list[str]], list[Tuple[int, int, int]]]:
+
+    stacks: list[list[str]] = []
+    moves: list[Tuple[int, int, int]] = []
+
+    for line in Path(p).read_text().splitlines():
+
+        if match := re.match(r'move (\d+) from (\d+) to (\d+)', line):
+            moves.append((int(match[1]), int(match[2]), int(match[3])))
+        elif '[' not in line:
+            continue
+        else:
+            for stack_num, crate in enumerate(line[1::4]):
+                if len(stacks) <= stack_num:
+                    stacks.append([])
+
+                if crate == ' ':
+                    continue
+
+                stacks[stack_num].insert(0, crate)
+
+    return stacks, moves
+
+stacks, moves = read('input.txt')
+
+print(stacks)
+print(moves)
+
+for quantity, source, dest in moves:
+
+    moved_crates = stacks[source-1][-(quantity):]
+
+    for _ in range(quantity):
+        stacks[source-1].pop()
+
+    stacks[dest-1].extend(moved_crates)
+
+    print(stacks)
+
+print(''.join(s.pop() for s in stacks))
+

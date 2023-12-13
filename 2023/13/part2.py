37929d6e8c6a6b98ac72ce74906ab3a9d2984ff9
Daniel Flook <daniel@flook.org>
2023-12-13 13:56:23 +0000

    Day 13

---
+++ 2023/13/part2.py
@@ -0,0 +1,113 @@
+from pathlib import Path
+from typing import Iterable, Optional
+
+Field = list[str]
+def parse_fields(input: str) -> Iterable[Field]:
+    field = []
+
+    for line in input.splitlines():
+        if not line and field:
+            yield field
+            field = []
+            continue
+        field.append(line)
+
+    yield field
+
+def is_smudge(a: str, b: str) -> bool:
+    repair = False
+    for c_a, b_c in zip(a, b):
+        if c_a == b_c:
+            continue
+
+        if repair:
+            return False
+
+        repair = True
+
+    return True
+
+def is_reflection(field: Field, reflection_line: int) -> bool:
+
+    print(f'Is there a reflection after line {reflection_line}')
+
+    smudge_repaired = False
+
+    for l_num, r_num in [(reflection_line - i, reflection_line + i + 1) for i in range(len(field))]:
+
+        if r_num >= len(field):
+            print('reflection')
+            return smudge_repaired
+        if l_num < 0:
+            print('reflection')
+            return smudge_repaired
+
+        print(l_num, r_num)
+
+        l = field[l_num]
+        r = field[r_num]
+
+        print(l)
+        print(r)
+
+        if l == r:
+            continue
+
+        if smudge_repaired:
+            return False
+
+        # Possibly a smudge
+        if is_smudge(l, r):
+            smudge_repaired = True
+            print('repaired smudge')
+            continue
+
+        if l != r:
+            print('No reflection')
+            return False
+
+        print('-')
+
+    print('reflection')
+    return smudge_repaired
+
+def transpose(field: Field) -> Field:
+    return [''.join(column) for column in zip(*field)]
+
+def find_reflection(field: Field) -> Optional[int]:
+    for i in range(len(field) - 1):
+        if is_reflection(field, i):
+            return (i + 1) * 100
+
+    field = transpose(field)
+
+    for i in range(len(field) - 1):
+        if is_reflection(field, i):
+            return (i + 1)
+
+    return None
+
+def answer(input: str) -> int:
+    s = 0
+
+    for field in parse_fields(input):
+        print(field)
+        for line in field:
+            print(line)
+
+        if (i := find_reflection(field)) is not None:
+            s += i
+            continue
+
+        print('transposed')
+        for line in transpose(field):
+            print(line)
+
+        if (i := find_reflection(field)) is not None:
+            s += i
+
+    return s
+
+
+#print(answer(Path('test.txt').read_text()))
+print(answer(Path('input.txt').read_text()))
